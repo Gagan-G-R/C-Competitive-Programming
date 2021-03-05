84ea26850ee373ff83b1cf5110bc1b74d718a25a
Gagan G R <gagan2001j@gmail.com>
2021-03-05 13:20:18 +0530

    reversing an array

--- reversing_Array.c
+++ reversing_Array.c
@@ -1,4 +1,24 @@
 #include<stdio.h>
+void disp(int *a,int n){
+    for(int i=0;i<n;i++){
+        printf("a[%d]=%d\n",i,a[i]);
+    }
+}
 void main(){
-    printf("Hello world");
+    int n;
+    int a[100];
+    int temp;
+    printf("enter the lenght of the array");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        printf("Enter the element %d : ",i);
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n/2;i++){
+        temp=a[i];
+        a[i]=a[n-i-1];
+        a[n-i-1]=temp;
+    }
+    disp(a,n);
+
 }
